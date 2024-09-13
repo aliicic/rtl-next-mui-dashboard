deacb1d7c26ac4a14de266481f2e2c8c446b94e0
aliicic <aliaghakhani911@gmail.com>
2024-09-13 23:54:25 +0330

    ADD env

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,14 @@ import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 // import "../../public/irancell/font.css";
 import irancell from "../utils/constants/localFont";
+// import type { Metadata } from 'next'
+
+
+// export const metadata: Metadata = {
+//   title: 'My Dashboard',
+//   description: '...',
+// }
+
 
 export default function RootLayout({
   children,
@@ -11,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" dir="rtl" className={`${irancell.variable} font-sans `} >
+    <html lang="fa " dir="rtl" className={`${irancell.variable} font-sans `} >
       <body>
         <ThemeProvider theme={baselightTheme}>
           {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
