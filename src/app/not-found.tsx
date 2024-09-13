dcf2edd9cccdd3283d62d404b3af414a57475a54
aliicic <aliaghakhani911@gmail.com>
2024-09-13 22:23:09 +0330

    ADD not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,16 @@
+const NotFound = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <h1>Not Found 404</h1>
+    </div>
+  );
+};
+
+export default NotFound;
